f5c3aba678194f43b67661cbf508d0055e8c922f
Sharon Segal <sharon.j.segal@gmail.com>
2021-08-16 11:46:05 +0300

    Hotfix: Javascript

--- scripts/script.js
+++ scripts/script.js
@@ -5,17 +5,24 @@ let formElement = document.querySelector('.form');
 let editProfileBtn = pageContainer.querySelector('.profile__edit');
 let closeEditorBtn = page.querySelector('.modal__close-btn');
 
-
-function toggleEditor () {
-    modal.classList.toggle('modal_opened');
-}
-
 let inputName = formElement.querySelector('.form__text-input_type_name');
 let inputTitle = formElement.querySelector('.form__text-input_type_title');
 let profileName = pageContainer.querySelector('.profile__name');
 let profileTitle = pageContainer.querySelector('.profile__title');
-inputName.value = profileName.textContent;
-inputTitle.value = profileTitle.textContent;
+
+function openEditor() {
+    let isOpened = modal.classList.add('modal_opened');
+    
+    if(isOpened = true) {
+    inputName.value = profileName.textContent;
+    inputTitle.value = profileTitle.textContent;
+}
+}
+
+function closeEditor () {
+    modal.classList.remove('modal_opened');
+}
+
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
@@ -23,9 +30,10 @@ function handleFormSubmit(evt) {
     profileName.textContent = `${inputName.value}`;
     profileTitle.textContent = `${inputTitle.value}`;
 
-    modal.classList.toggle('modal_opened');
+    modal.classList.remove('modal_opened');
 }
 
-editProfileBtn.addEventListener('click', toggleEditor);
-closeEditorBtn.addEventListener('click', toggleEditor);
+editProfileBtn.addEventListener('click', openEditor);
+closeEditorBtn.addEventListener('click', closeEditor);
 formElement.addEventListener('submit', handleFormSubmit);
+
