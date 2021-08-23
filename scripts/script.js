2dc6ad211480b992c04002f438b932bbd03de1e9
Sharon Segal <sharon.j.segal@gmail.com>
2021-08-23 12:43:46 +0300

    hotfix: made cards fixed width even when there are less than three, refactored event listerners

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+import initialCards from "./initialCards.js";
+
 const modalEditForm = document.querySelector('.modal_type_edit-profile');
 const modalAddCard = document.querySelector('.modal_type_add-element');
 const modalImage = document.querySelector('.modal_type_image');
@@ -19,9 +21,6 @@ const cardsContainer = document.querySelector('.cards__container');
 
 const cardTemplate = document.querySelector('#card-template').content;
 const cardElement = cardTemplate.querySelector('.card__container').cloneNode(true);
-const title = cardElement.querySelector('.card__name');
-
-
 
 
 function handleEditor () {
@@ -46,61 +45,43 @@ function handleFormSubmit(evt) {
 
 modalEditForm.addEventListener('submit', handleFormSubmit);
 
-const initialCards = [
-    {
-        name: 'Yosemite Valley',
-        link: 'https://images.unsplash.com/photo-1472396961693-142e6e269027?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2103&q=80'},
-    {
-        name: 'Lake Louise',
-        link: 'https://images.unsplash.com/photo-1581088382991-83c7f170de75?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80'},
-    {
-        name: 'Bald Mountains',
-        link: 'https://images.unsplash.com/photo-1601025252036-36faf914679d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2001&q=80'},
-    {
-        name: 'Latemar',
-        link: 'https://images.unsplash.com/photo-1539540706191-b673a0a5b6d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80'},
-    {
-        name: 'Vanoise National Park',
-        link: 'https://images.unsplash.com/photo-1549752448-38f4d94420f5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'},
-    {
-        name: 'Lago di Braies',
-        link: 'https://images.unsplash.com/photo-1620497998791-64bb5dc460b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2145&q=80'}
-]
 
 
+const deleteCard = (evt) => {
+    evt.target.closest('.card__container').remove();
+}
+
+const likeCard = (evt) => {
+    evt.target.classList.toggle('card__like-button_active');
+}
+
+const openImageGallery = (evt) => {
+    modalImage.classList.toggle('modal_opened');
+    const imageModalTemplate = document.querySelector('#image-template').content;
+    const modalContainer = imageModalTemplate.querySelector('.modal__container').cloneNode(true);
+    modalImage.append(modalContainer);
+    const targetSrc = evt.target.parentElement.querySelector('.card__image').getAttribute('src');
+    const targetName = evt.target.parentElement.querySelector('.card__name').textContent;
+    modalContainer.querySelector('.card__image').setAttribute('src', targetSrc);
+    modalContainer.querySelector('.card__image').setAttribute('alt', targetName);
+    modalContainer.querySelector('.card__name').textContent = targetName;
+
+    modalContainer.querySelector('.modal__close-btn').addEventListener('click', () => {
+    modalImage.classList.toggle('modal_opened');
+    modalContainer.remove();
+    })
+}
 
 initialCards.forEach(card => {
-    const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card__container').cloneNode(true);
     cardElement.querySelector('.card__name').textContent = card.name;
     cardElement.querySelector('.card__image').setAttribute('src', card.link);
     cardElement.querySelector('.card__image').setAttribute('alt', card.name);
-    cardsContainer.appendChild(cardElement);
-
-    cardElement.querySelector('.card__delete-button').addEventListener('click', (evt) => {
-        evt.target.closest('.card__container').remove();
-      })
-   
-    cardElement.querySelector('.card__like-button').addEventListener('click', (evt) => {
-        evt.target.classList.toggle('card__like-button_active');
-      })
+    cardsContainer.append(cardElement);
     
-    cardElement.querySelector('.card__image').addEventListener('click', (evt) => {
-        modalImage.classList.toggle('modal_opened');
-        const imageModalTemplate = document.querySelector('#image-template').content;
-        const modalContainer = imageModalTemplate.querySelector('.modal__container').cloneNode(true);
-        modalImage.append(modalContainer);
-        const targetSrc = evt.target.parentElement.querySelector('.card__image').getAttribute('src');
-        const targetName = evt.target.parentElement.querySelector('.card__name').textContent;
-        modalContainer.querySelector('.card__image').setAttribute('src', targetSrc);
-        modalContainer.querySelector('.card__image').setAttribute('alt', targetName);
-        modalContainer.querySelector('.card__name').textContent = targetName;
-
-        modalContainer.querySelector('.modal__close-btn').addEventListener('click', () => {
-        modalImage.classList.toggle('modal_opened');
-        modalContainer.remove();
-        })
-    })
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard)
+    cardElement.querySelector('.card__like-button').addEventListener('click', likeCard)
+    cardElement.querySelector('.card__image').addEventListener('click', openImageGallery)
 })
 
 function openCardForm () {
@@ -116,39 +97,15 @@ function openCardForm () {
 
 addCardBtn.addEventListener('click', openCardForm);
 
-
-function createCard (titleValue, urlValue){
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.card__container').cloneNode(true);
-    cardElement.querySelector('.card__name').textContent = titleValue;
-    cardElement.querySelector('.card__image').setAttribute('src', urlValue);
-    cardElement.querySelector('.card__image').setAttribute('alt', titleValue);
+function createCard (title, url){
+    cardElement.querySelector('.card__name').textContent = title;
+    cardElement.querySelector('.card__image').setAttribute('src', url);
+    cardElement.querySelector('.card__image').setAttribute('alt', title);
     cardsContainer.prepend(cardElement);
     
-    cardElement.querySelector('.card__delete-button').addEventListener('click', (evt) => {
-        evt.target.closest('.card__container').remove();
-      })
-
-    cardElement.querySelector('.card__like-button').addEventListener('click', (evt) => {
-        evt.target.classList.toggle('card__like-button_active');
-      })
-
-    cardElement.querySelector('.card__image').addEventListener('click', (evt) => {
-        modalImage.classList.toggle('modal_opened');
-        const imageModalTemplate = document.querySelector('#image-template').content;
-        const modalContainer = imageModalTemplate.querySelector('.modal__container').cloneNode(true);
-        modalImage.append(modalContainer);
-        const targetSrc = evt.target.parentElement.querySelector('.card__image').getAttribute('src');
-        const targetName = evt.target.parentElement.querySelector('.card__name').textContent;
-        modalContainer.querySelector('.card__image').setAttribute('src', targetSrc);
-        modalContainer.querySelector('.card__image').setAttribute('alt', targetName);
-        modalContainer.querySelector('.card__name').textContent = targetName;
-
-        modalContainer.querySelector('.modal__close-btn').addEventListener('click', () => {
-        modalImage.classList.toggle('modal_opened');
-        modalContainer.remove();
-        })
-    })
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard)
+    cardElement.querySelector('.card__like-button').addEventListener('click', likeCard)
+    cardElement.querySelector('.card__image').addEventListener('click', openImageGallery)
 }
 
 modalAddCard.addEventListener('submit', (evt) => {
@@ -156,3 +113,5 @@ modalAddCard.addEventListener('submit', (evt) => {
     createCard(inputTitle.value, inputUrl.value);
     modalAddCard.classList.remove('modal_opened');
 })   
+
+
