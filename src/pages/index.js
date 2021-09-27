57697a111b0af5306420b0215ed64ddbe8c32e63
Sharon Segal <sharon.j.segal@gmail.com>
2021-09-27 16:17:23 +0300

    feat: module configuration and webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
+import '../pages/index.css';
+import logoSrc from '../images/logo.svg';
+import profileImgSrc from '../images/profile_image.jpg';
+import addImgSrc from '../images/add.svg';
+
+document.querySelector('.logo').src = logoSrc;
+document.querySelector('.profile__image').src = profileImgSrc;
+document.querySelector('.add__icon').src = addImgSrc;
+
 import Card from '../components/Card.js';
 import Section from '../components/Section.js';
 import PopupWithImage from '../components/PopupWithImage.js';
